073f0a6775c2c02cadcb63cb4e2cc35b83b4bc5d
nikschen <niklaswiemuth@gmail.com>
2021-12-21 19:53:18 +0100

    not null annotationen gesetzt

--- app/src/main/java/com/fourquestionmarks/food_o_mat/model/Meal.kt
+++ app/src/main/java/com/fourquestionmarks/food_o_mat/model/Meal.kt
@@ -2,17 +2,29 @@ package com.fourquestionmarks.food_o_mat.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
 
 @Entity (tableName="meals")
 data class Meal(
+    @NotNull
     @PrimaryKey (autoGenerate = true)
     val ID:Int?=0,
+    @NotNull
     var name: String,
+    @NotNull
     var category: String,
+    @NotNull
     var calories: Float,
+    @NotNull
     var carbohydrates: Float,
+    @NotNull
     var proteins: Float,
+    @NotNull
     var fats: Float,
-    var isVeggie: Boolean)
+    @NotNull
+    var isVeggie: Boolean,
+    @NotNull
+    var isVegan: Boolean,
+    )
 {
 }
