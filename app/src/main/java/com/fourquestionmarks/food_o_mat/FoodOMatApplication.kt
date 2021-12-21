d3b6ea3dfedc5d05824ce2ef85333b9b51c4ba49
nikschen <niklaswiemuth@gmail.com>
2021-12-21 19:46:21 +0100

    Callback für Database Prefill eingebaut

--- app/src/main/java/com/fourquestionmarks/food_o_mat/FoodOMatApplication.kt
+++ app/src/main/java/com/fourquestionmarks/food_o_mat/FoodOMatApplication.kt
@@ -2,8 +2,11 @@ package com.fourquestionmarks.food_o_mat
 
 import android.app.Application
 import com.fourquestionmarks.food_o_mat.data.FoodOMatDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class FoodOMatApplication : Application()  {
 
-    val database: FoodOMatDatabase by lazy { FoodOMatDatabase.getDatabase(this) }
+    val applicationScope= CoroutineScope(SupervisorJob())
+    val database: FoodOMatDatabase by lazy { FoodOMatDatabase.getDatabase(this, applicationScope) }
 }
--- app/src/main/java/com/fourquestionmarks/food_o_mat/data/MealDatabase.kt
+++ app/src/main/java/com/fourquestionmarks/food_o_mat/data/MealDatabase.kt
@@ -4,7 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.fourquestionmarks.food_o_mat.model.Meal
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.time.Instant
+import java.time.ZoneId
 
 @Database(entities = [Meal::class], version = 2, exportSchema = false)
 abstract class FoodOMatDatabase : RoomDatabase() {
@@ -15,7 +20,7 @@ abstract class FoodOMatDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: FoodOMatDatabase? = null
 
-        fun getDatabase(context: Context): FoodOMatDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): FoodOMatDatabase {
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
@@ -27,11 +32,32 @@ abstract class FoodOMatDatabase : RoomDatabase() {
                     // Wipes and rebuilds instead of migrating if no Migration object.
                     // Migration is not part of this codelab.
                     .fallbackToDestructiveMigration()
+                    .addCallback(FOMDatabaseCallback(scope))
                     .build()
                 INSTANCE = instance
                 // return instance
-                instance
+                return instance
             }
+        }
+    }
+
+    //callback for prefilling the database with test data
+    private class FOMDatabaseCallback(private val scope: CoroutineScope): RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabaseWithMeals(database.mealDao())
+                }
+            }
+        }
+
+
+        // prefilling database functions for each entity
+        suspend fun populateDatabaseWithMeals(mealDao: MealDao) {
+
+            val meal = Meal(0, "Testname", "Auflauf", 123.4f, 43.21f, 12.3f, 10.5f, true, false)
+            mealDao.insert(meal)
         }
     }
 }
