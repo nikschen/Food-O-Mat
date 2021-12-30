984126ffb252e0d23fc6672145cf5c8549ca7a05
nikschen <niklaswiemuth@gmail.com>
2021-12-30 13:18:14 +0100

    KeyValueStore als settings bekannt gemacht

--- app/src/main/java/com/fourquestionmarks/food_o_mat/FoodOMatApplication.kt
+++ app/src/main/java/com/fourquestionmarks/food_o_mat/FoodOMatApplication.kt
@@ -2,11 +2,15 @@ package com.fourquestionmarks.food_o_mat
 
 import android.app.Application
 import com.fourquestionmarks.food_o_mat.data.FoodOMatDatabase
+import com.fourquestionmarks.food_o_mat.data.KeyValueStore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
 class FoodOMatApplication : Application()  {
 
-    val applicationScope= CoroutineScope(SupervisorJob())
+    private val applicationScope= CoroutineScope(SupervisorJob())
     val database: FoodOMatDatabase by lazy { FoodOMatDatabase.getDatabase(this, applicationScope) }
+    val settings: KeyValueStore= KeyValueStore(this)
+
+
 }
